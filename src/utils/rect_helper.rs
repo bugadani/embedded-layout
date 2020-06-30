b1f92bfd8aa606a606e7f745de377993229b7998
Dániel Buga <bugadani@gmail.com>
2020-06-30 14:45:27 +0200

    New helper: enveloping rectangle

--- src/utils/rect_helper.rs
+++ src/utils/rect_helper.rs
@@ -30,6 +30,7 @@ pub trait RectExt {
     /// *Note:* when an object's width or height is an even number, the returned center point will
     ///         not be perfectly in the middle.
     fn center(&self) -> Point;
+    fn enveloping(&self, other: &Rectangle) -> Rectangle;
 }
 
 impl RectExt for Rectangle {
@@ -64,6 +65,39 @@ impl RectExt for Rectangle {
 
     fn center(&self) -> Point {
         Point::new(self.center_x(), self.center_y())
+    }
+
+    fn enveloping(&self, other: &Rectangle) -> Rectangle {
+        Rectangle::new(
+            Point::new(
+                self.top_left.x.min(other.top_left.x),
+                self.top_left.y.min(other.top_left.y),
+            ),
+            Point::new(
+                self.bottom_right.x.max(other.bottom_right.x),
+                self.bottom_right.y.max(other.bottom_right.y),
+            ),
+        )
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::prelude::*;
+    use embedded_graphics::{prelude::*, primitives::Rectangle};
+
+    #[test]
+    fn test_enveloping() {
+        let rect0 = Rectangle::new(Point::new(-1, -1), Point::new(0, 0));
+        let rect1 = Rectangle::new(Point::zero(), Point::new(1, 1));
+        let rect2 = Rectangle::new(Point::zero(), Point::new(2, 2));
+
+        assert_eq!(rect2, rect2.enveloping(&rect1));
+        assert_eq!(rect2, rect1.enveloping(&rect2));
+        assert_eq!(
+            Rectangle::new(Point::new(-1, -1), Point::new(2, 2),),
+            rect0.enveloping(&rect2)
+        );
     }
 }
 
