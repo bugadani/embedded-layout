86fd5c46348e7b736aba98e5e15a0d5904b1e7e6
Dániel Buga <bugadani@gmail.com>
2020-07-03 10:24:47 +0200

    Replace example with the one using simulator

--- src/lib.rs
+++ src/lib.rs
@@ -50,31 +50,48 @@
 //! Use `LinearLayout` to arrange multiple objects:
 //!
 //! ```rust
-//! # use embedded_graphics::mock_display::MockDisplay;
-//! # let mut disp: MockDisplay<BinaryColor> = MockDisplay::new();
-//! #
-//! use embedded_layout::prelude::*;
-//! use embedded_layout::layout::linear::LinearLayout;
-//! use embedded_graphics::{
-//!     fonts::{Font6x8, Text},
-//!     pixelcolor::BinaryColor,
-//!     style::TextStyleBuilder,
+//! use embedded_graphics_simulator::{
+//!     BinaryColorTheme, OutputSettingsBuilder, SimulatorDisplay, Window,
 //! };
 //!
-//! let display_area = disp.display_area();
+//! use embedded_graphics::{
+//!     fonts::{Font6x8, Text},
+//!     geometry::Point,
+//!     pixelcolor::BinaryColor,
+//!     prelude::*,
+//!     style::TextStyleBuilder,
+//! };
+//! use embedded_layout::layout::linear::LinearLayout;
+//! use embedded_layout::prelude::*;
 //!
-//! let text_style = TextStyleBuilder::new(Font6x8)
-//!                         .text_color(BinaryColor::On)
-//!                         .build();
+//! fn main() -> Result<(), core::convert::Infallible> {
+//!     let mut display: SimulatorDisplay<BinaryColor> = SimulatorDisplay::new(Size::new(64, 48));
+//!     let output_settings = OutputSettingsBuilder::new()
+//!         .theme(BinaryColorTheme::OledBlue)
+//!         .build();
 //!
-//! LinearLayout::vertical()
-//!             .add_view(Text::new("Hello,", Point::zero()).into_styled(text_style))
-//!             .add_view(Text::new("World!", Point::zero()).into_styled(text_style))
-//!             .arrange()
-//!             .align_to(&display_area, horizontal::Center, vertical::Center)
-//!             .draw(&mut disp)
-//!             .unwrap();
+//!     let display_area = display.display_area();
+//!
+//!     let text_style = TextStyleBuilder::new(Font6x8)
+//!         .text_color(BinaryColor::On)
+//!         .build();
+//!
+//!     LinearLayout::vertical()
+//!         .with_alignment(horizontal::Center)
+//!         .add_view(Text::new("Vertical", Point::zero()).into_styled(text_style))
+//!         .add_view(Text::new("Linear", Point::zero()).into_styled(text_style))
+//!         .add_view(Text::new("Layout", Point::zero()).into_styled(text_style))
+//!         .arrange()
+//!         .align_to(&display_area, horizontal::Center, vertical::Center)
+//!         .draw(&mut display)
+//!         .unwrap();
+//!
+//!     Window::new("LinearLayout exmaple", &output_settings).show_static(&display);
+//!     Ok(())
+//! }
 //! ```
+//!
+//! This example is based on [the embedded-graphics simulator](https://github.com/jamwaffles/embedded-graphics/tree/master/simulator) and can be run using `cargo run --example linear_layout`
 //!
 //! [`embedded-graphics`]: https://github.com/jamwaffles/embedded-graphics/
 
