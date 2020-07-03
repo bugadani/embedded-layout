8b33b928931d01bbda2b7741160c8e1448c194d4
Dániel Buga <bugadani@gmail.com>
2020-07-03 10:02:55 +0200

    Add example using simulator

---
+++ examples/centered_text.rs
@@ -0,0 +1,33 @@
+use embedded_graphics_simulator::{
+    BinaryColorTheme, OutputSettingsBuilder, SimulatorDisplay, Window,
+};
+
+use embedded_graphics::{
+    fonts::{Font6x8, Text},
+    geometry::Point,
+    pixelcolor::BinaryColor,
+    prelude::*,
+    style::TextStyleBuilder,
+};
+use embedded_layout::prelude::*;
+
+fn main() -> Result<(), core::convert::Infallible> {
+    let mut display: SimulatorDisplay<BinaryColor> = SimulatorDisplay::new(Size::new(129, 129));
+    let display_area = display.display_area();
+
+    let text_style = TextStyleBuilder::new(Font6x8)
+        .text_color(BinaryColor::On)
+        .build();
+
+    Text::new("Hello, World!", Point::zero())
+        .into_styled(text_style)
+        .align_to(&display_area, horizontal::Center, vertical::Center)
+        .draw(&mut display)
+        .unwrap();
+
+    let output_settings = OutputSettingsBuilder::new()
+        .theme(BinaryColorTheme::OledBlue)
+        .build();
+    Window::new("Hello World", &output_settings).show_static(&display);
+    Ok(())
+}
