01e6821c91464029f9ddf5ecfea374237964d109
Dániel Buga <bugadani@gmail.com>
2020-07-05 00:19:47 +0200

    Deduplicate layout operations

--- src/layout/linear/layout_direction.rs
+++ src/layout/linear/layout_direction.rs
@@ -1,11 +1,27 @@
 use crate::{
-    align::{HorizontalAlignment, VerticalAlignment},
+    align::{Alignment, HorizontalAlignment, VerticalAlignment},
     layout::linear::secondary_alignment::SecondaryAlignment,
     prelude::*,
 };
 
 /// Helper trait that describes a layout direction.
-pub trait LayoutDirection: Copy + Clone {}
+pub trait LayoutDirection: Copy + Clone {
+    ///
+    type FirstHorizontalAlignment: HorizontalAlignment;
+
+    ///
+    type HorizontalAlignment: HorizontalAlignment;
+
+    ///
+    type FirstVerticalAlignment: VerticalAlignment;
+
+    ///
+    type VerticalAlignment: VerticalAlignment;
+
+    ///
+    type Secondary: SecondaryAlignment + Alignment;
+}
+
 /// Horizontal layout direction
 #[derive(Copy, Clone)]
 pub struct Horizontal<Secondary: SecondaryAlignment + VerticalAlignment> {
@@ -21,9 +37,15 @@ impl Default for Horizontal<vertical::Bottom> {
     }
 }
 
-impl<Secondary> LayoutDirection for Horizontal<Secondary> where
-    Secondary: SecondaryAlignment + VerticalAlignment
+impl<Secondary> LayoutDirection for Horizontal<Secondary>
+where
+    Secondary: SecondaryAlignment + VerticalAlignment,
 {
+    type FirstHorizontalAlignment = horizontal::Left;
+    type HorizontalAlignment = horizontal::LeftToRight;
+    type FirstVerticalAlignment = Secondary;
+    type VerticalAlignment = Secondary;
+    type Secondary = Secondary;
 }
 
 /// Vertical layout direction
@@ -41,7 +63,13 @@ impl Default for Vertical<horizontal::Left> {
     }
 }
 
-impl<Secondary> LayoutDirection for Vertical<Secondary> where
-    Secondary: SecondaryAlignment + HorizontalAlignment
+impl<Secondary> LayoutDirection for Vertical<Secondary>
+where
+    Secondary: SecondaryAlignment + HorizontalAlignment,
 {
+    type FirstHorizontalAlignment = Secondary;
+    type HorizontalAlignment = Secondary;
+    type FirstVerticalAlignment = vertical::Top;
+    type VerticalAlignment = vertical::TopToBottom;
+    type Secondary = Secondary;
 }
--- src/layout/linear/layout_operation.rs
+++ src/layout/linear/layout_operation.rs
@@ -1,25 +1,22 @@
 use crate::{
-    align::{HorizontalAlignment, VerticalAlignment},
+    align::Alignment,
     layout::{ChainTerminator, ViewChainElement, ViewLink},
     prelude::*,
 };
 use embedded_graphics::primitives::Rectangle;
 
-use super::{
-    layout_direction::{Horizontal, LayoutDirection, Vertical},
-    secondary_alignment::SecondaryAlignment,
-};
+use super::{layout_direction::LayoutDirection, secondary_alignment::SecondaryAlignment};
 
 pub trait LayoutOperation<LD: LayoutDirection> {
     fn measure(&self) -> Size;
     fn arrange(&mut self, bounds: Rectangle) -> Rectangle;
 }
 
-impl<V, VCE, Secondary> LayoutOperation<Horizontal<Secondary>> for ViewLink<V, VCE>
+impl<V, VCE, LD> LayoutOperation<LD> for ViewLink<V, VCE>
 where
     V: View + Align,
-    VCE: ViewChainElement + LayoutOperation<Horizontal<Secondary>>,
-    Secondary: SecondaryAlignment + VerticalAlignment,
+    VCE: ViewChainElement + LayoutOperation<LD>,
+    LD: LayoutDirection,
 {
     fn measure(&self) -> Size {
         let current_el_size = self.view.size();
@@ -27,72 +24,31 @@ where
             current_el_size
         } else {
             let prev_size = self.next.measure();
-            Secondary::measure(prev_size, current_el_size)
+            LD::Secondary::measure(prev_size, current_el_size)
         }
     }
 
     fn arrange(&mut self, bounds: Rectangle) -> Rectangle {
         if VCE::IS_TERMINATOR {
-            self.view
-                .align_to_mut(&bounds, horizontal::Left, Secondary::new());
+            self.view.align_to_mut(
+                &bounds,
+                LD::FirstHorizontalAlignment::new(),
+                LD::FirstVerticalAlignment::new(),
+            );
         } else {
             let previous = self.next.arrange(bounds);
 
-            self.view
-                .align_to_mut(&previous, horizontal::LeftToRight, Secondary::new());
+            self.view.align_to_mut(
+                &previous,
+                LD::HorizontalAlignment::new(),
+                LD::VerticalAlignment::new(),
+            );
         }
         self.view.bounds()
     }
 }
 
-impl<Secondary> LayoutOperation<Horizontal<Secondary>> for ChainTerminator
-where
-    Secondary: SecondaryAlignment + VerticalAlignment,
-{
-    fn measure(&self) -> Size {
-        Size::new(0, 0)
-    }
-
-    fn arrange(&mut self, _bounds: Rectangle) -> Rectangle {
-        // Nothing to do
-        Rectangle::new(Point::zero(), Point::zero())
-    }
-}
-
-impl<V, VCE, Secondary> LayoutOperation<Vertical<Secondary>> for ViewLink<V, VCE>
-where
-    V: View + Align,
-    VCE: ViewChainElement + LayoutOperation<Vertical<Secondary>>,
-    Secondary: SecondaryAlignment + HorizontalAlignment,
-{
-    fn measure(&self) -> Size {
-        let current_el_size = self.view.size();
-        if VCE::IS_TERMINATOR {
-            current_el_size
-        } else {
-            let prev_size = self.next.measure();
-            Secondary::measure(prev_size, current_el_size)
-        }
-    }
-
-    fn arrange(&mut self, bounds: Rectangle) -> Rectangle {
-        if VCE::IS_TERMINATOR {
-            self.view
-                .align_to_mut(&bounds, Secondary::new(), vertical::Top);
-        } else {
-            let previous = self.next.arrange(bounds);
-
-            self.view
-                .align_to_mut(&previous, Secondary::new(), vertical::TopToBottom);
-        }
-        self.view.bounds()
-    }
-}
-
-impl<Secondary> LayoutOperation<Vertical<Secondary>> for ChainTerminator
-where
-    Secondary: SecondaryAlignment + HorizontalAlignment,
-{
+impl<LD: LayoutDirection> LayoutOperation<LD> for ChainTerminator {
     fn measure(&self) -> Size {
         Size::new(0, 0)
     }
