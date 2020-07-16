de408ef7e9492bceacd79773ded63eb3d09ff260
Dániel Buga <bugadani@gmail.com>
2020-07-16 20:06:29 +0200

    Add a helper macro to object chains and a method to append to the chain

--- src/utils/object_chain.rs
+++ src/utils/object_chain.rs
@@ -6,12 +6,21 @@
 //! `Guard` to make this structure more useful.
 
 /// A generic chain element
-pub trait ChainElement {
+pub trait ChainElement: Sized {
     /// `true` if this chain element marks the end of a chain
     const IS_TERMINATOR: bool;
 
     /// Return the number of objects linked to this chain element
     fn count() -> u32;
+
+    /// Append an object to the chain
+
+    fn append<T>(self, item: T) -> Link<T, Self> {
+        Link {
+            object: item,
+            next: self,
+        }
+    }
 }
 
 /// This piece of the chain contains some object
@@ -41,5 +50,40 @@ impl ChainElement for Guard {
     #[inline]
     fn count() -> u32 {
         0
+    }
+}
+
+/// Helper macro to make working with object chains easier
+#[macro_export]
+macro_rules! chain {
+    () => {Guard};
+    ($x:tt) => {Link<$x, Guard>};
+    ($x:tt, $($rest:tt),*) => {Link<$x, chain! { $($rest),* }>};
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    struct CompileTest {
+        empty_chain: chain! {},
+        chain1: chain! {
+            u8
+        },
+        chain: chain! {
+            u8, u16, u32
+        },
+    }
+
+    #[test]
+    pub fn test() {
+        fn f(_obj_chain: &chain! {u8, u16, u32}) {}
+
+        let test = CompileTest {
+            empty_chain: Guard,
+            chain1: Guard.append(0),
+            chain: Guard.append(0).append(1).append(2),
+        };
+
+        f(&test.chain);
     }
 }
